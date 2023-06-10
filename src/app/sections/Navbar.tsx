3294a1ddc44389d789a00f298ec7702b7adbf9a1
Jose J. Cruz <jjzcru@gmail.com>
2023-06-10 13:17:23 -0500

    refactor: :lipstick: Increase size of header

--- src/app/sections/Navbar.tsx
+++ src/app/sections/Navbar.tsx
@@ -3,7 +3,7 @@ import styles from '../page.module.css';
 export default function Navbar() {
 	return (
 		<nav id={'navbar'} className={styles.navbar}>
-			<div>Gatun</div>
+			<div>Gatun Sport Fishing</div>
 			<button>Book Now</button>
 		</nav>
 	);
