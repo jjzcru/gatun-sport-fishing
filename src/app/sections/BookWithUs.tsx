8b18cd260524ef0b2b09cf3a594c6c08e7b39c60
Jose J. Cruz <jjzcru@gmail.com>
2023-07-01 01:01:09 -0500

    feat: :sparkles: Replace booking with us image with a video

--- src/app/sections/BookWithUs.tsx
+++ src/app/sections/BookWithUs.tsx
@@ -1,32 +1,31 @@
-import { useContext } from "react";
-import { AppContext } from "../App.context";
+import { useContext } from 'react';
+import { AppContext } from '../App.context';
 
-import styles from "../page.module.css";
+import styles from '../page.module.css';
 
 export default function BookWithUs() {
-    const { bookWithUs } = useContext(AppContext);
-    const { title, content, image } = bookWithUs;
-    const style = {
-        backgroundImage: `url("${image}")`
-    };
+	const { bookWithUs } = useContext(AppContext);
+	const { title, content, video } = bookWithUs;
 
-    return (
-        <section className={styles["book-with-us"]}>
-            <div>
-                <div className={styles["image"]}>
-                    <img style={style} />
-                </div>
-                <div className={styles["content"]}>
-                    <div>
-                        <h2>{title}</h2>
-                        <div className={styles['divider']} />
-                        {content.map((c: string, i: number) => (
-                            <p key={`p-${i}`}>{c}</p>
-                        ))}
-                    </div>
-                </div>
-
-            </div>
-        </section>
-    );
+	return (
+		<section className={styles['book-with-us']}>
+			<div>
+				<div className={styles['media']}>
+					<video autoPlay loop playsInline muted>
+						<source src={video} type="video/mp4" />
+					</video>
+					<div className={styles.overlay} />
+				</div>
+				<div className={styles['content']}>
+					<div>
+						<h2>{title}</h2>
+						<div className={styles['divider']} />
+						{content.map((c: string, i: number) => (
+							<p key={`p-${i}`}>{c}</p>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
